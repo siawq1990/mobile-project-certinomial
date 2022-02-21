14a300ebf29dac67bae20bcbcb19b03a2192a0fc
AouIssa <51041563+AouIssa@users.noreply.github.com>
2022-02-21 22:47:49 +0800

    UI Colors

---
+++ lib/interfces/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final Color facebookColor = const Color(0xff39579A);
+final Color twitterColor = const Color(0xff00ABEA);
+final Color instaColor = const Color(0xffBE2289);
+final Color whatsappColor = const Color(0xff075E54);
+final Color linkedinColor = const Color(0xff0085E0);
+final Color githubColor = const Color(0xff202020);
+final Color googleColor = const Color(0xffDF4A32);
